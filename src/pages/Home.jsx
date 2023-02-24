51651767915d4219349f37e4a1980794c2846095
haliltuzcu <007canberk@gmail.com>
2023-02-24 15:39:03 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AddTutorial from "../components/AddTutorial";
+import TutorialList from "../components/TutorialList";
+
+const Home = () => {
+const [tutorials, setTutorials] = useState([])
+
+  const BASE_URL="https://tutorial-api.fullstack.clarusway.com/tutorials/"
+  const getTutorials = async () => {
+    try {
+      const {data} = await axios(BASE_URL)
+      setTutorials(data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+   getTutorials()
+  }, [])
+  
+  return (
+    <>
+      <AddTutorial />
+      <TutorialList tutorials={tutorials} getTutorials={getTutorials} />
+    </>
+  );
+};
+
+export default Home;
